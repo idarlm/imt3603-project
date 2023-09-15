a317084ca38c0dd3b9bdcd991ab5c811c79416df
idarlm <idar.mykloey99@gmail.com>
2023-09-15 13:13:46 +0200

    Add script structure

--- Assets/Scripts/Player/MovementHandler.cs
+++ Assets/Scripts/Player/MovementHandler.cs
@@ -1,13 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-public class MovementHandler : MonoBehaviour
+namespace Player
 {
 
-    void Start()
+    [RequireComponent(typeof(CharacterController))]
+    public class MovementHandler : MonoBehaviour
     {
+        // Public properties
+        public CharacterController Controller { get; private set; }
+
+        public Vector3 Velocity => Controller.velocity;
+        public Vector3 OldVelocity { get; private set; }
+
+        // Private fields
+        private Vector3 _groundNormal = Vector3.up;
+
+        // Unity messages
+        private void Start()
+        {
+            Controller = GetComponent<CharacterController>();
+        }
+
+        private void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+            if (hit.controller.collisionFlags.HasFlag(CollisionFlags.Below))
+            {
+                _groundNormal = hit.normal;
+            }
+        }
+        
+        // Public methods
+        public void Move(Vector3 movement)
+        {
+            
+        }
+
+        public void SetVelocity(Vector3 velocity)
+        {
+            
+        }
+
+        public void AddVelocity(Vector3 velocity)
+        {
+            
+        }
         
     }
+
 }
--- Assets/Scripts/Player/PlayerMovementSystem.cs
+++ Assets/Scripts/Player/PlayerMovementSystem.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MovementHandler))]
-public class PlayerMovementSystem : MonoBehaviour
+namespace Player
 {
-    
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void FixedUpdate()
+    [RequireComponent(typeof(MovementHandler))]
+    public class PlayerMovementSystem : MonoBehaviour
     {
 
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+
+        }
     }
+
 }
