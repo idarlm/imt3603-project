6c153ad5a19d68d0be502bb0a010e5a3512feae8
idarlm <idar.mykloey99@gmail.com>
2023-09-15 14:44:32 +0200

    Implement basic movement functionality

--- Assets/Scripts/Player/PlayerMovementSystem.cs
+++ Assets/Scripts/Player/PlayerMovementSystem.cs
@@ -8,12 +8,21 @@ namespace Player
     [RequireComponent(typeof(MovementHandler))]
     public class PlayerMovementSystem : MonoBehaviour
     {
-        private interface IMovementState
+        private abstract class MovementState
         {
-            public void Enter();        // Called when entering state
-            public void Exit();         // Called when exiting state
-            public void Update();       // Called every update cycle
-            public void HandleInput();  // Used to handle inputs/state transitions
+            public MovementState(PlayerMovementSystem system)
+            {
+                MovementSystem = system;
+                Handler = system._handler;
+            }
+
+            public PlayerMovementSystem MovementSystem { get; private set; }
+            public MovementHandler Handler { get; private set; }
+
+            public abstract void Enter();        // Called when entering state
+            public abstract void Exit();         // Called when exiting state
+            public abstract void Update();       // Called every update cycle
+            public abstract void HandleInput();  // Used to handle inputs/state transitions
         }
         
         public float Speed = 5f;
@@ -22,13 +31,14 @@ namespace Player
         private MovementHandler _handler;
 
         private bool _changeState = false;
-        private IMovementState _currentState;
-        private IMovementState _nextState;
+        private MovementState _currentState;
+        private MovementState _nextState;
         
         // Unity messages
         private void Start()
         {
             _handler = GetComponent<MovementHandler>();
+            _currentState = new GroundedState(this);
         }
 
         private void Update()
@@ -45,17 +55,100 @@ namespace Player
                 _currentState.Exit();
                 _currentState = _nextState;
                 _currentState.Enter();
+
+                _changeState = false;
             }
 
             _currentState.Update();
         }
         
         // Private methods
-        private void ChangeState(IMovementState newState)
+        private void ChangeState(MovementState newState)
         {
             _nextState = newState;
             _changeState = true;
         }
+        
+        
+        // State classes
+        private class GroundedState : MovementState
+        {
+            public GroundedState(PlayerMovementSystem pms) : base(pms)
+            {
+            }
+
+
+            public override void Enter()
+            {
+                Debug.Log("Enter grounded state");
+            }
+
+            public override void Exit()
+            {
+                Debug.Log("Exit grounded state");
+            }
+
+            public override void Update()
+            {
+                var dt = Time.fixedDeltaTime;
+                var movement = Vector3.right * dt;
+                movement += Vector3.up * (MovementSystem.Gravity * dt * dt);
+
+                if (Handler.ShouldStick)
+                {
+                    movement += Vector3.down;
+                }
+                
+                Handler.Move(movement);
+            }
+
+            public override void HandleInput()
+            {
+                bool grounded = Handler.Grounded || Handler.ShouldStick;
+
+                if (!grounded)
+                {
+                    MovementSystem.ChangeState(new FallingState(MovementSystem));
+                }
+            }
+        }
+
+        private class FallingState : MovementState
+        {
+            public FallingState(PlayerMovementSystem pms) : base(pms)
+            {
+            }
+
+
+            public override void Enter()
+            {
+                Debug.Log("Entered FallingState");
+            }
+
+            public override void Exit()
+            {
+                Debug.Log("Exit GroundedState");
+            }
+
+            public override void Update()
+            {
+                float dt = Time.fixedDeltaTime;
+                Vector3 movement = Handler.Velocity * dt;
+                movement += Vector3.up * (MovementSystem.Gravity * dt * dt);
+                
+                Handler.Move(movement);
+            }
+
+            public override void HandleInput()
+            {
+                if (Handler.Grounded)
+                {
+                    MovementSystem.ChangeState(new GroundedState(MovementSystem));
+                    return;
+                }
+            }
+        }
+        
     }
 
 }
