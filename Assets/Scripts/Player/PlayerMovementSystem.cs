124459c26c4e274627c7d34fe89c9886442de4b8
idarlm <idar.mykloey99@gmail.com>
2023-09-15 14:01:52 +0200

    Update PlayerMovementSystem.cs
    Implemented state management and basic properties/fields.

--- Assets/Scripts/Player/PlayerMovementSystem.cs
+++ Assets/Scripts/Player/PlayerMovementSystem.cs
@@ -8,21 +8,53 @@ namespace Player
     [RequireComponent(typeof(MovementHandler))]
     public class PlayerMovementSystem : MonoBehaviour
     {
+        private interface IMovementState
+        {
+            public void Enter();        // Called when entering state
+            public void Exit();         // Called when exiting state
+            public void Update();       // Called every update cycle
+            public void HandleInput();  // Used to handle inputs/state transitions
+        }
+        
+        public float Speed = 5f;
+        public float Gravity = -10f;
 
+        private MovementHandler _handler;
 
-        void Start()
+        private bool _changeState = false;
+        private IMovementState _currentState;
+        private IMovementState _nextState;
+        
+        // Unity messages
+        private void Start()
+        {
+            _handler = GetComponent<MovementHandler>();
+        }
+
+        private void Update()
         {
 
         }
 
-        void Update()
+        private void FixedUpdate()
         {
+            _currentState.HandleInput();
 
+            if (_changeState)
+            {
+                _currentState.Exit();
+                _currentState = _nextState;
+                _currentState.Enter();
+            }
+
+            _currentState.Update();
         }
-
-        void FixedUpdate()
+        
+        // Private methods
+        private void ChangeState(IMovementState newState)
         {
-
+            _nextState = newState;
+            _changeState = true;
         }
     }
 
