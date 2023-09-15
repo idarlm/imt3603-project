c87ae18b32f8a602afff280fa3b84937a6276121
idarlm <idar.mykloey99@gmail.com>
2023-09-15 13:47:08 +0200

    Update MovementHandler.cs
    Implement basic move method

--- Assets/Scripts/Player/MovementHandler.cs
+++ Assets/Scripts/Player/MovementHandler.cs
@@ -15,6 +15,13 @@ namespace Player
         public Vector3 Velocity => Controller.velocity;
         public Vector3 OldVelocity { get; private set; }
 
+        public bool Grounded => Controller.isGrounded;
+        public bool ShouldStick { get; private set; }
+
+        // Public fields
+        public float StickyThreshold = 0.4f;
+        public float StickyRadius = 0.2f;
+
         // Private fields
         private Vector3 _groundNormal = Vector3.up;
 
@@ -35,7 +42,14 @@ namespace Player
         // Public methods
         public void Move(Vector3 movement)
         {
+            OldVelocity = Controller.velocity;
             
+            Controller.Move(movement);
+            
+            // Check if we should stick to the ground
+            Ray r = new(transform.position, Vector3.down);
+            float dist = (Controller.height * 0.5f) + StickyThreshold;
+            ShouldStick = Physics.SphereCast(r, StickyRadius, dist);
         }
 
         public void SetVelocity(Vector3 velocity)
