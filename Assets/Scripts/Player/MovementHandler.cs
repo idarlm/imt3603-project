3b42a9783da9b0634053969dca490f6eee109beb
idarlm <idar.mykloey99@gmail.com>
2023-09-15 11:50:12 +0200

    Add script files

---
+++ Assets/Scripts/Player/MovementHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementHandler : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MovementHandler))]
+public class PlayerMovementSystem : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
