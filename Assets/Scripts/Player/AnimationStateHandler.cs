af48adf36eab03322a63bb123019b1e3daec1eba
andrrma <andrrma@stud.ntnu.no>
2023-09-22 14:28:29 +0200

    updating character animation

--- Assets/Scripts/Player/AnimationStateHandler.cs
+++ Assets/Scripts/Player/AnimationStateHandler.cs
@@ -12,6 +12,8 @@ namespace Player
         [SerializeField] private Animator _characterAnimator;
 
         private static readonly int IsMoving = Animator.StringToHash("isMoving");
+        private static readonly int IsFalling = Animator.StringToHash("isFalling");
+        private static readonly int IsJumping = Animator.StringToHash("isJumping");
 
         // Start is called before the first frame update
         void Start()
@@ -22,7 +24,13 @@ namespace Player
         // Update is called once per frame
         void Update()
         {
+            var notTouchingGround = _playerController.Falling;
+            var movingUp = _playerController.Velocity.y > 0;
+            _characterAnimator.SetBool(IsJumping, movingUp && notTouchingGround);
             _characterAnimator.SetBool(IsMoving, _playerController.Velocity.magnitude != 0);
+            _characterAnimator.SetBool(IsFalling, notTouchingGround);
+            _characterAnimator.SetBool(IsFalling, notTouchingGround);
+            
             var frontVector = Vector3.ProjectOnPlane(_playerController.Velocity.normalized, Vector3.up);
             
             if (frontVector != Vector3.zero)
