ccac0b721f5857a21b5f419ae4d568796d245ff4
andrrma <andrrma@stud.ntnu.no>
2023-09-22 11:47:08 +0200

    Alters sliding doors back to not utilizing LeanTween

--- Assets/Scripts/EventSystem/SlidingDoorController.cs
+++ Assets/Scripts/EventSystem/SlidingDoorController.cs
@@ -39,37 +39,41 @@ namespace EventSystem
         {
             if (_moving)
             {
-                /*
                 _animationState = Math.Clamp(
                     _animationState + Time.deltaTime * (_opening? 1 / openTime : -1 / closeTime), 0, 1);
                 transform.position = Vector3.Lerp(
                     _startPosition, _startPosition + endPositionOffset, _animationState);
                 _moving = (transform.position != _startPosition);
-                */
             }
         }
 
         
         private void OnDoorwayClose(int id)
         {
-            //_opening = !_triggerStatus.SetStatus(id, false);;
+            _moving = true;
+            _opening = !_triggerStatus.SetStatus(id, false);
             //var remainingTime = Time.time - LeanTween.descr(_tweenID).time;
+            /*
             if (_tween != null && _animationState != 0.0f) _animationState -= 
                 (openTime * _animationState - _tween.passed) / (openTime * _animationState);
             LeanTween.cancel(gameObject);
             _tween = LeanTween.move(gameObject, _startPosition, closeTime * _animationState);
+            */
         }
 
         private void OnDoorwayOpen(int id)
         {
-            //_opening = !_triggerStatus.SetStatus(id, false);;
+            _triggerStatus.SetStatus(id, true);
+            _moving = _opening = _triggerStatus.AllActive();
             //var remainingTime = Time.time - LeanTween.descr(_tweenID).time;
-            if (_tween != null && _animationState != 1.0f) _animationState -= 
+            /*
+            if (_tween != null && _animationState != 1.0f) _animationState -=
                 (closeTime * (1.0f - _animationState) - _tween.passed) / closeTime * (1.0f-_animationState);
             LeanTween.cancel(gameObject);
             _tween = LeanTween
                 .move(gameObject, _startPosition + endPositionOffset, openTime * (1.0f-_animationState))
                 .setEaseInOutCubic();
+                */
         }
     }
 }
