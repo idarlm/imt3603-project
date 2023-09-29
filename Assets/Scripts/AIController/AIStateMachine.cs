4a27a2b35c9f332cf4f3b6d5fcfe221a4e9befe9
andrrma <andrrma@stud.ntnu.no>
2023-09-29 16:00:31 +0200

    Adds name identifier and a factory class for AI chase states

--- Assets/Scripts/AIController/AIStateMachine.cs
+++ Assets/Scripts/AIController/AIStateMachine.cs
@@ -1,25 +1,39 @@
-﻿using StateMachine;
+﻿using System.Collections.Generic;
+using AIController.ChaseBehaviour;
+using LabMaterials;
+using StateMachine;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 namespace AIController
 {
     public class AIStateMachine : StateMachineMono<AIContext>
     {
+        [SerializeField] private string currentStateSerialized;
         private IState<AIContext> _currentState;
         private AIContext _context;
 
-        [SerializeField] private IState<AIContext>[] _states;
-        public enum AIState
-        {
-            Chasing = 0,
-            Patrolling = 1,
-            Idle = 2
-        }
+        [SerializeField] private string chaseState;
+
+        [SerializeField] private Transform target;
         
         
         private void Start()
         {
-            _currentState = _states[(int)AIState.Idle];
+            _currentState = ChaseFactory.CreateChaseState(currentStateSerialized);
+            this.
+            _context = new AIContext
+            {
+                Agent = GetComponent<NavMeshAgent>(),
+                Target = target.transform
+            };
+        }
+
+        public void ChangeState(IAIState nextState)
+        {
+            base.ChangeState(nextState);
+            currentStateSerialized = nextState.GetName();
         }
 
         private void Update()
@@ -27,4 +41,5 @@ namespace AIController
             _currentState?.Update(_context);
         }
     }
+    
 }
