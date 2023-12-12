3c3cf796da41c43536b087ab54f3001ce5aeb537
andrrma <andrrma@stud.ntnu.no>
2023-12-12 11:43:52 +0100

    Exposes player illumination through GetIllumination method on PlayerIlluminationController.cs

--- Assets/Scripts/Illumination/PlayerIlluminationController.cs
+++ Assets/Scripts/Illumination/PlayerIlluminationController.cs
@@ -15,11 +15,21 @@ namespace Illumination
         public Transform head;
 
         public Transform chest;
+
+        private PlayerIllumination _illumination;
         
         // Start is called before the first frame update
         void Start()
         {
         
+        }
+
+        private void Update()
+        {
+            _illumination.LeftHandIllumination = IlluminationManager.Instance.GetIllumination(leftHand.position,HumanBodyBones.LeftHand);
+            _illumination.RightHandIllumination = IlluminationManager.Instance.GetIllumination(rightHand.position,HumanBodyBones.RightHand);
+            _illumination.HeadIllumination = IlluminationManager.Instance.GetIllumination(head.position,HumanBodyBones.Head);
+            _illumination.ChestIllumination = IlluminationManager.Instance.GetIllumination(chest.position,HumanBodyBones.Chest);
         }
 
         // Update is called once per frame
@@ -30,10 +40,16 @@ namespace Illumination
 
         private void OnDrawGizmosSelected()
         {
+            _illumination.HeadIllumination = IlluminationManager.Instance.GetIllumination(head.position,HumanBodyBones.Head);
             Handles.Label(leftHand.position, Math.Round(IlluminationManager.Instance.GetIllumination(leftHand.position,HumanBodyBones.LeftHand), 2).ToString());
             Handles.Label(rightHand.position, Math.Round(IlluminationManager.Instance.GetIllumination(rightHand.position,HumanBodyBones.RightHand), 2).ToString());
             Handles.Label(head.position, Math.Round(IlluminationManager.Instance.GetIllumination(head.position,HumanBodyBones.Head), 2).ToString());
             Handles.Label(chest.position, Math.Round(IlluminationManager.Instance.GetIllumination(chest.position,HumanBodyBones.Chest), 2).ToString());
+        }
+        
+        public PlayerIllumination GetIllumination()
+        {
+            return _illumination;
         }
     }
 }
