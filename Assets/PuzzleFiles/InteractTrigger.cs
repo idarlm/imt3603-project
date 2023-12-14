de1969235ef23bc572f1bb0e0bfc0f27e9013ee3
marthsja <127052202+marthsja@users.noreply.github.com>
2023-12-14 11:18:28 +0100

    comments, animateonevent + interacttrigger + simpletrigger

--- Assets/PuzzleFiles/InteractTrigger.cs
+++ Assets/PuzzleFiles/InteractTrigger.cs
@@ -1,29 +1,26 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class InteractTrigger : PuzzleTrigger {
 
-    private bool inRange = false;
+    private bool inRange = false;   //indicate if player is ini range or not
 
     private void Update() {
-
+        //continuously check if key E is pressed and if player is in range
         if (Input.GetKeyDown(KeyCode.E) && inRange) {
             FireTriggered(this, EventArgs.Empty);
-            Debug.Log("InteractTrigger fired");
         }
     }
 
+    // When entering the box collider, check if the collider is the player, and set inRange to true
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.name == "Player") { 
             inRange = true;
-            InteractionTextUI.ShowUI();
+            InteractionTextUI.ShowUI(); //text showing indicating interaction option
         }
     }
 
+    // When exiting the box collider, check if the collider is the player, and set inRange to false
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.name == "Player") {
